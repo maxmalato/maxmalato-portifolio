e06d88bb7c8b0cc6fef8d8cba4b04f781e348dbe
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-02-28 14:13:50 -0300

    feat:criação do comentário finalizado.

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -27,7 +27,7 @@ const FeedbackForm = ({ editingFeedback, setEditingFeedback }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 dark:bg-slate-700 dark:text-slate-100">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 dark:bg-slate-700 dark:text-slate-100">
             <h2 className="font-bold text-3xl text-center">{editingFeedback ? "Editar Comentário" : "Adicionar Comentário"}</h2>
             <input
                 type="text"
@@ -44,7 +44,7 @@ const FeedbackForm = ({ editingFeedback, setEditingFeedback }) => {
                 className="p-2 border rounded-lg dark:bg-slate-800 dark:border-slate-600"
                 required
             />
-            <button type="submit" className="p-2 rounded-lg bg-orange-600 text-white w-96 font-semibold self-center">
+            <button type="submit" className="p-2 rounded-lg bg-orange-600 text-white w-80 font-semibold self-center">
                 {editingFeedback ? "Salvar" : "Enviar"}
             </button>
         </form>
